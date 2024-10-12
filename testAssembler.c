9dab466205ffb59cbe999fa5a89b448012c217e1
kaewtar <kaewtarcmu@gmail.com>
2024-10-12 15:19:39 +0700

    update testing command

--- testAssembler.c
+++ testAssembler.c
@@ -1,9 +1,34 @@
 /*run command
+    -version 1- by Kanolas -
     ./assembler input.txt output.txt
     gcc readAndParse.cpp -o assembler
 
+    -version 2- by Kaewtar -
     g++ demoAssembler.c -o demoAssembler
     ./demoAssembler input2.txt output2.txt
+
+    -version 3 - by Thiranut -
+        ถ้ามี assembler.exe แล้วก็
+        assembler.exe mult.txt machineCode.txt
+        g++  behav.txt -o behav
+        behav.exe 
+        ไฟล์ผลลัพธ์จะออกที่ simulationOutput.txt
+
+    -version 4 - by Thiranut -
+    1. g++ assembler.cpp -o ชื่อไรก็ได้มันจะได้ไฟล์ ชื่อนั้น.exe
+    2. .\ชื่อ.exe ชื่อไฟล์ assemblyที่เขียน.txt machineCode.txt
+    3. ไปกดรันไฟล์ behavior
+    4. output ออกไฟล์ simulationOutput.txt
+
+    *** warning ***
+    1.ทำครั้งเดียวพอ
+    2.ค่อยทำใหม่ถ้าโค้ด assembly เปลี่ยน
+    ระวังเรื่องlabel ยาวเกิน7ตัวด้วยนะ
+
+    -version 5 - by Kaewtar -
+    g++ assembler.cpp -o assembler    
+    ./assembler.exe mult.txt machineCode.txt
+    ./behav behav.txt simulationOutput.txt
 */
 
 /*  input 1:
